1ac26a284ca1ab2983b675a79b8b9424e918f29c
zhaoxuyang <zhaoxuyang@songguo7.com>
2019-09-24 15:02:48 +0800

    单例模式

--- common/src/main/java/net/zhaoxuyang/common/pattern/singleton/Singleton.java
+++ common/src/main/java/net/zhaoxuyang/common/pattern/singleton/Singleton.java
@@ -1,22 +1,22 @@
-/**
- * 单例模式
- * 1.Singleton最多只有一个实例，在不考虑反射强行突破访问限制的情况下。
- * 2.保证了并发访问的情况下，不会发生由于并发而产生多个实例。
- * 3.保证了并发访问的情况下，不会由于初始化动作未完全完成而造成使用了尚未正确初始化的实例。
- */
+/*
+单例模式（Sinleton Pattern）定义：
+Ensure a class has only one instance, and provide a global point of access to it.
+（确保一个类只有一个实例，而且自行实例化并向系统提供这个实例）
+*/
 package net.zhaoxuyang.common.pattern.singleton;
 
-/**
- *
- * @author zhaoxuyang
- */
-public class Singleton {
+public class Singleton {    
     
-    private Singleton(){}
-    public Singleton getInstance(){
-        return SingletonInstance.instance;
-    }
-    public static class SingletonInstance{
+    private static class SingletonInstance{
         static Singleton instance = new Singleton();
     }
+    private Singleton(){}
+    public static Singleton getInstance(){
+        return SingletonInstance.instance;
+    }
+
+    public void method(){
+        System.out.println("执行单例模式的一个公共方法");
+    }
+    
 }
