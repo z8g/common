1d853eb48cdc8591f1a6c5a2631391133a84259b
zhaoxuyang <zhaoxuyang@songguo7.com>
2019-10-04 22:28:00 +0800

    工具-搜索-二分搜索

--- algorithms/src/main/java/util/Search.java
+++ algorithms/src/main/java/util/Search.java
@@ -1,10 +1,20 @@
 package util;
 
+/**
+ * 包含一些常见的搜索算法
+ * @author zhaoxuyang
+ */
 public class Search {
 
     private Search() {
     }
 
+    /**
+     * 二分搜索
+     * @param a
+     * @param key
+     * @return 
+     */
     public static int binarySearch(int[] a, int key) {
         int low = 0;
         int high = a.length - 1;
