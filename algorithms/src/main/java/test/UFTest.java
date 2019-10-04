f1f31384f4dd79dd8dbf891968de6cdd391a7e33
zhaoxuyang <zhaoxuyang@songguo7.com>
2019-10-04 20:40:33 +0800

    测试-Union Find算法

---
+++ algorithms/src/main/java/test/UFTest.java
@@ -0,0 +1,64 @@
+package test;
+
+import ds.UF;
+import ds.impl.DefaultUF;
+import ds.impl.QuickFindUF;
+import ds.impl.QuickUnionUF;
+
+/**
+ * Union Find算法
+ * @author zhaoxuyang
+ */
+public class UFTest {
+
+    public static void main(String[] args) {
+        UF uf;
+        int findResult;
+
+        uf = new QuickFindUF(10);
+        uf.union(4, 3);
+        uf.union(3, 8);
+        uf.union(6, 5);
+        uf.union(9, 4);
+        uf.union(2, 1);
+        uf.union(5, 0);
+        uf.union(7, 2);
+        uf.union(6, 1);
+        findResult = uf.find(3);
+        System.out.println(findResult);
+        System.out.println(uf.size());
+        System.out.println(uf.connected(4, 3));
+        System.out.println(uf.connected(9, 3));
+
+        uf = new DefaultUF(10);
+        uf.union(4, 3);
+        uf.union(3, 8);
+        uf.union(6, 5);
+        uf.union(9, 4);
+        uf.union(2, 1);
+        uf.union(5, 0);
+        uf.union(7, 2);
+        uf.union(6, 1);
+        findResult = uf.find(3);
+        System.out.println(findResult);
+        System.out.println(uf.size());
+        System.out.println(uf.connected(4, 3));
+        System.out.println(uf.connected(9, 3));
+
+        uf = new QuickUnionUF(10);
+        uf.union(4, 3);
+        uf.union(3, 8);
+        uf.union(6, 5);
+        uf.union(9, 4);
+        uf.union(2, 1);
+        uf.union(5, 0);
+        uf.union(7, 2);
+        uf.union(6, 1);
+        findResult = uf.find(3);
+        System.out.println(findResult);
+        System.out.println(uf.size());
+        System.out.println(uf.connected(4, 3));
+        System.out.println(uf.connected(9, 3));
+
+    }
+}
