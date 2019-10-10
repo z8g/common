10d2ce1077e4aafa40c9e85966ef7b7b24add765
zhaoxuyang <zhaoxuyang@songguo7.com>
2019-10-10 16:00:53 +0800

    Netty - Echo

---
+++ demo/src/main/java/net/zhaoxuyang/common/nio/netty/echo/EchoClient.java
@@ -0,0 +1,59 @@
+package net.zhaoxuyang.common.nio.netty.echo;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import java.net.InetSocketAddress;
+
+/**
+ * 客户端
+ *
+ * @author hadoop
+ */
+public class EchoClient {
+
+    private final String host;
+    private final int port;
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap boot = new Bootstrap();
+            boot.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture future = boot.connect().sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException ex) {
+            System.out.println(ex);
+        } finally {
+            try {
+                group.shutdownGracefully().sync();
+            } catch (InterruptedException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+    
+    public static void main(String[] args){
+        String host = "127.0.0.1";
+        int port = 8080;
+        new EchoClient(host,port).start();
+    }
+
+}
---
+++ demo/src/main/java/net/zhaoxuyang/common/nio/netty/echo/EchoClientHandler.java
@@ -0,0 +1,48 @@
+package net.zhaoxuyang.common.nio.netty.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 客户端的处理逻辑
+ *
+ * 客户端将会： （1） 连接到服务器 （2） 发送一个或者多个消息 （3） 对于每条消息，等待并接收从服务器发回的相同的消息 （4） 关闭连接
+ *
+ * @author hadoop
+ */
+@Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    /**
+     * 需要实现的方法，记录已收到的消息的转储
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        System.out.println("客户端收到：" + msg.toString(CharsetUtil.UTF_8));
+    }
+
+    /**
+     * 当被通知Channel是活跃的时候，发送一条消息
+     *
+     * @param ctx
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty 连接", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        System.out.println(cause);
+        ctx.close();
+    }
+
+}
