440f1ad4f6b7b3603fe76a14923b1cce2d9b8418
zhaoxuyang <zhaoxuyang@songguo7.com>
2019-10-10 16:12:47 +0800

    分离出demo

--- common/src/main/java/net/zhaoxuyang/common/method/rmi/pca/PcaService.java
+++
@@ -1,8 +0,0 @@
-package net.zhaoxuyang.common.method.rmi.pca;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface PcaService  extends Remote{
-    public double[][] pca(double [][] datas,Double threshold) throws RemoteException;
-}
