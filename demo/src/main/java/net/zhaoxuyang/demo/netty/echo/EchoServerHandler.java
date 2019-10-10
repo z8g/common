5e2554b06d6936377929dc2e22e3254be118ee0c
zhaoxuyang <zhaoxuyang@songguo7.com>
2019-10-10 16:11:41 +0800

    Netty(Echo+文件上传)

---
+++ demo/src/main/java/net/zhaoxuyang/demo/netty/echo/EchoServerHandler.java
@@ -0,0 +1,48 @@
+package net.zhaoxuyang.demo.netty.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 服务端的处理逻辑
+ * 所有的Netty服务器都需要以下两部分：
+ * - 至少一个 ChannelHandler ，业务逻辑
+ * - 引导程序——配置服务器的启动代码，监听端口
+ * @author hadoop
+ */
+@Sharable//标示一个ChannelHandler可以被多个Channel安全地共享
+public class EchoServerHandler extends ChannelInboundHandlerAdapter{
+    
+    /**
+     * 将接收到的消息写给发送者，而不冲刷出站消息
+     * @param ctx
+     * @param msg 
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx,Object msg){
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println(in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+    
+    /**
+     * 将 pending message 冲刷到远程节点，并且关闭Channel
+     * @param ctx 
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx){
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+    
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause){
+        System.out.println(cause);
+        ctx.close();
+    }
+}
---
+++ demo/src/main/java/net/zhaoxuyang/demo/netty/fileupload/FileClient.java
@@ -0,0 +1,59 @@
+package net.zhaoxuyang.demo.netty.fileupload;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import java.net.InetSocketAddress;
+
+/**
+ * 客户端
+ *
+ * @author hadoop
+ */
+public class FileClient {
+
+    private final String host;
+    private final int port;
+
+    public FileClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap boot = new Bootstrap();
+            boot.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new FileClientHandler());
+                        }
+                    });
+            ChannelFuture future = boot.connect().sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException ex) {
+            System.out.println(ex);
+        } finally {
+            try {
+                group.shutdownGracefully().sync();
+            } catch (InterruptedException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+    
+    public static void main(String[] args){
+        String host = "127.0.0.1";
+        int port = 8080;
+        new FileClient(host,port).start();
+    }
+
+}
