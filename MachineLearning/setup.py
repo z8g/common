8d61edc8abdb7ac68abbe4539b2166961597f488
zhaoxuyang <zhaoxuyang@songguo7.com>
2019-10-26 17:28:59 +0800

    机器学习

---
+++ MachineLearning/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup (
+       name='MachineLearning',
+       version='0.1',
+       packages=find_packages(),
+
+       # Declare your packages' dependencies here, for eg:
+       install_requires=['foo>=3'],
+
+       # Fill in these to make your Egg ready for upload to
+       # PyPI
+       author='zhaoxuyang',
+       author_email='zhaoxuyang19971015@foxmail.com',
+
+       #summary = 'Just another Python package for the cheese shop',
+       url='https://zhaoxuyang.net/',
+       license='',
+       long_description='machine learning | https://github.com/z8g',
+       
+       # could also include long_description, download_url, classifiers, etc.
+
+  
+       )
