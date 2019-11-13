8547c8ff14f6113a9b9d1d6b2508ac558b9bbb84
zhaoxuyang <zhaoxuyang@songguo7.com>
2019-11-13 11:36:10 +0800

    限流测试

--- demo/src/main/java/net/zhaoxuyang/demo/rate/GuavaTest.java
+++ demo/src/main/java/net/zhaoxuyang/demo/rate/GuavaTest.java
@@ -12,12 +12,13 @@ import java.util.logging.Logger;
  */
 public class GuavaTest {
     private static final Logger LOG = Logger.getLogger(GuavaTest.class.getName());
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String start = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-        RateLimiter limiter = RateLimiter.create(1.0); // 这里的1表示每秒允许处理的量为1个
-        for (int i = 1; i <= 10; i++) {
-            limiter.acquire();// 请求RateLimiter, 超过permits会被阻塞
-            LOG.log(Level.INFO, "call: {0}", i);
+        RateLimiter limiter = RateLimiter.create(1000); // 这里的1表示每秒允许处理的量为1个
+        for (int i = 1; i <= 30; i++) {
+            double ac = limiter.acquire(100);// 请求RateLimiter, 超过permits会被阻塞
+            Thread.sleep((int)Math.random() * 40 + 20);//
+            LOG.log(Level.INFO, "call: {0}",i);
         }
         String end = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
         LOG.log(Level.INFO, "start time:{0}", start);
