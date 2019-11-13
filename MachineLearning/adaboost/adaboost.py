66620d4febd21af399b35dfb30a131e3697b5902
zhaoxuyang <zhaoxuyang@songguo7.com>
2019-11-13 20:34:12 +0800

    AdaBoost

--- MachineLearning/adaboost/adaboost.py
+++ MachineLearning/adaboost/adaboost.py
@@ -1,6 +1,15 @@
 # -*- coding: UTF-8 -*-
 
+from numpy import exp
+from numpy import inf
+from numpy import log
+from numpy import mat
 from numpy import matrix
+from numpy import multiply
+from numpy import ones
+from numpy import shape
+from numpy import sign
+from numpy import zeros
 """
 ================================================================================
 元算法：对其他算法组合的一种方式
@@ -43,15 +52,116 @@ AdaBoost运行过程：
 ================================================================================
 """
 
+"""
+加载数据
+"""
 def loadSimpleData():
 	dataMat = matrix([
-		[1.0, 2.1],
-		[2.0, 1.1],
-		[1.3, 1.0],
-		[1.0, 1.0],
-		[2.0, 1.1]
-	])
+					 [1.0, 2.1],
+					 [2.0, 1.1],
+					 [1.3, 1.0],
+					 [1.0, 1.0],
+					 [2.0, 1.1]
+					 ])
 	classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
 	return dataMat, classLabels
 
+"""
+单层决策树生成函数
+"""
+def stumpClassify(dataMatrix, dimen, threshVal, threshIneq):
+	retArray = ones((shape(dataMatrix)[0], 1))
+	if threshIneq == 'lt':
+		retArray[dataMatrix[:, dimen] <= threshVal] = -1.0
+	else:
+		retArray[dataMatrix[:, dimen] > threshVal] = -1.0
+	return retArray
+	
+def buildStump(dataArr, classLabels, D):
+	dataMatrix = mat(dataArr);
+	labelMat = mat(classLabels).T
+	m, n = shape(dataMatrix)
+	numSteps = 10.0
+	bestStump = {}
+	bestClassEst = mat(zeros((m, 1)))
+	minError = inf
+	for i in range(n):
+		rangeMin = dataMatrix[:, i].min()
+		rangeMax = dataMatrix[:, i].max()
+		stepSize = (rangeMax-rangeMin) / numSteps
+		for j in range(-1, int(numSteps) + 1):
+			for inequal in ['lt', 'gt']:
+				threshVal = (rangeMin + float(j) * stepSize)
+				predictedVals = stumpClassify(dataMatrix, i, threshVal, inequal)
+				errArr = mat(ones((m, 1)))
+				errArr[predictedVals == labelMat] = 0
+				weightedError = D.T * errArr
+				if weightedError < minError:
+					minError = weightedError
+					bestClassEst = predictedVals.copy()
+					bestStump['dim'] = i
+					bestStump['thresh'] = threshVal
+					bestStump['ineq'] = inequal
+	return bestStump, minError, bestClassEst
 
+"""
+							AdaBoost训练单层决策树
+================================================================================
+对每次迭代：
+	利用buildStump()函数找到最佳的单层决策树
+	将最佳单层决策树加入到单层决策树数组
+	计算alpha
+	计算新的权重向量D
+	更新累计类别估计值
+	如果错误率等于0.0，则退出循环
+================================================================================
+"""
+def adaBoostTrainDS(dataArr, classLabels, numIt=40):
+	weakClassArr = []
+	m = shape(dataArr)[0]
+	D = mat(ones((m, 1)) / m)
+	aggClassEst = mat(zeros((m, 1))) # 保持一个运行时估计值 
+	for i in range(numIt):
+		print "index:", i
+		bestStump, error, classEst = buildStump(dataArr, classLabels, D)
+		print "D:", D.T
+		# max(error,1e-16) 确保在没有错误时不会发生除零溢出
+		alpha = float(0.5 * log((1.0-error) / max(error, 1e-16)))
+		bestStump['alpha'] = alpha
+		weakClassArr.append(bestStump)
+		print "classEst: ", classEst.T
+		
+		expon = multiply(-1 * alpha * mat(classLabels).T, classEst)
+		D = multiply(D, exp(expon))
+		D = D / D.sum()
+		aggClassEst += alpha * classEst
+		print "aggClassEst: ", aggClassEst.T
+		
+		aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T, ones((m, 1)))
+		errorRate = aggErrors.sum() / m
+		print "total error: ", errorRate, "\n"
+		if errorRate == 0.0:
+			break
+	return weakClassArr
+
+"""
+AdaBoost分类函数
+"""
+def adaClassify(datToClass, classifierArr):
+	dataMatrix = mat(datToClass)
+	print "matrix{", dataMatrix , "}"
+	m = shape(dataMatrix)[0]
+	aggClassEst = mat(zeros((m, 1)))
+	for i in range(len(classifierArr)):
+		classEst = stumpClassify(dataMatrix,
+								 classifierArr[i]['dim'], 
+								 classifierArr[i]['thresh'], 
+								 classifierArr[i]['ineq'])
+		aggClassEst += classifierArr[i]['alpha']*classEst
+		# print aggClassEst
+	return sign(aggClassEst)
+
+
+
+	
+	
--- MachineLearning/adaboost/adaboost_test.py
+++ MachineLearning/adaboost/adaboost_test.py
@@ -1,16 +1,39 @@
 # -*- coding: UTF-8 -*-
 
 import adaboost
+from numpy import mat
+from numpy import ones
 import unittest
 
-
 class  AdaBoost_TestCase(unittest.TestCase):
-    def test_load_simple_data(self):
-		dataMat, classLabels=adaboost.loadSimpleData()
-		print dataMat, classLabels
-		
+   
+	def test_load_simple_data(self):
+		#print "test_load_simple_data"
+		dataMat, classLabels = adaboost.loadSimpleData()
+		#print dataMat, classLabels
+
+	def test_build_stump(self):
+		#print "test_build_stump"
+		dataMat, classLabels = adaboost.loadSimpleData()
+		D = mat(ones((5, 1)) / 5)
+		result = adaboost.buildStump(dataMat, classLabels, D)
+		#print result
 	
-		 
+	def test_adaboost_train_ds(self):
+		#print "test_adaboost_train_ds"
+		dataMat, classLabels = adaboost.loadSimpleData()
+		classifierArray = adaboost.adaBoostTrainDS(dataMat, classLabels, 9)
+		#print classifierArray
+	
+	def test_ada_classify(self):
+		print "test_ada_classify"
+		dataMat, classLabels = adaboost.loadSimpleData()
+		classifierArr = adaboost.adaBoostTrainDS(dataMat, classLabels, 9)
+		print "c1:", adaboost.adaClassify([0, 0], classifierArr)
+		print "c2:", adaboost.adaClassify([[5, 5], [0, 0]], classifierArr)
+		print "c2:", adaboost.adaClassify([[0, 0], [5, 5]], classifierArr)
+	
+
 if __name__ == '__main__':
     unittest.main()
 
