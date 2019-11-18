07ba6d940d3c6249c533dfb961e523aa01b87019
zhaoxuyang <zhaoxuyang@songguo7.com>
2019-11-18 12:38:46 +0800

    AdaBoost

--- MachineLearning/adaboost/adaboost_test.py
+++ MachineLearning/adaboost/adaboost_test.py
@@ -29,9 +29,29 @@ class  AdaBoost_TestCase(unittest.TestCase):
 		print "test_ada_classify"
 		dataMat, classLabels = adaboost.loadSimpleData()
 		classifierArr = adaboost.adaBoostTrainDS(dataMat, classLabels, 9)
-		print "c1:", adaboost.adaClassify([0, 0], classifierArr)
-		print "c2:", adaboost.adaClassify([[5, 5], [0, 0]], classifierArr)
-		print "c2:", adaboost.adaClassify([[0, 0], [5, 5]], classifierArr)
+#		print "c1:", adaboost.adaClassify([0, 0], classifierArr)
+#		print "c2:", adaboost.adaClassify([[5, 5], [0, 0]], classifierArr)
+#		print "c2:", adaboost.adaClassify([[0, 0], [5, 5]], classifierArr)
+		
+		
+	"""
+	从文件中加载数据
+	收集数据：提供的文本文件
+	准备数据：确保分类是+1和-1而非1和0
+	分析数据：手工检查数据
+	训练算法：训练分类器
+	测试算法：与逻辑回归比较
+	使用算法：观察错误率
+	"""
+	def test_loadDataSet(self):
+		dataArr, labelArr = adaboost.loadDataSet('train.txt')
+		print "[dataArr]", dataArr
+		print "[labelArr]",labelArr
+		classifierArray = adaboost.adaBoostTrainDS(dataArr, labelArr, 9)
+		testArr, testLabelArr = adaboost.loadDataSet('test.txt')
+		prediction10 = adaboost.adaClassify(testArr, classifierArray)
+		errArr = mat(ones((67, 1)))
+		print errArr[prediction10 != mat(testLabelArr).T].sum()
 	
 
 if __name__ == '__main__':
